632833624d75628bac74c234b775e6fb67b08f64
azizaweb <azizaweb@mail.ru>
2025-01-06 23:02:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// let name = prompt('Mars');-для инпута
+// let name = confitm('Mars');-для да или нет
+// console.log(name)
+let name = 'hello '
+ alert(name) 
+ let admin = prompt( "Сколько тебе лет?",   )
+ alert(`19 ${admin}`)
+ admin = prompt( "Как тебя зовут?", )
+ alert(`Aziza ${admin}`)
+ admin = prompt( "Какая у тебя фамилия?", )
+ alert(`Agayeva ${admin}`)
+ admin = prompt( "Где ты живешь?", )
+ alert(`YunusAbad ${admin}`)
